e993c822d924ed5473614103aca91b8e79f96139
John Hartman <john.f.d.hartman@gmail.com>
2017-09-19 14:58:47 -0700

    wrote auth code for session and user controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,26 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:username], params[:password])
+    if @user
+      login(user)
+      render "api/users#show"
+    else
+      render json: ['Invalid username or password'], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render "api/users#show"
+    else
+      render json: ['Nobody is signed in']
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
