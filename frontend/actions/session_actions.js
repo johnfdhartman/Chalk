a221fac6ef6591c2b5f58bc60dce0a851f65f1f5
John Hartman <john.f.d.hartman@gmail.com>
2017-09-19 16:16:23 -0700

    added session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as Api from '../util/auth_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser: user
+});
+
+export const receiveSessionErrors = (sessionErrors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  sessionErrors
+});
+
+export const signup = (user) => (dispatch) => (
+  Api.signup(user).then(
+    outUser => dispatch(receiveCurrentUser(outUser)),
+    errors => dispatch(receiveSessionErrors(errors))
+  )
+);
+
+
+export const login = (user) => (dispatch) => (
+  Api.login(user).then(
+    outUser => dispatch(receiveCurrentUser(outUser)),
+    errors => dispatch(receiveSessionErrors(errors))
+  )
+);
+
+export const logout = () => (dispatch) => (
+  Api.logout.then(
+    () => dispatch(receiveCurrentUser(null)),
+    (errors) => dispatch(receiveSessionErrors(errors))
+  )
+);
