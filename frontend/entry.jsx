3028400d91862186c21f81e60c1386785fb4d493
John Hartman <john.f.d.hartman@gmail.com>
2017-09-19 15:10:42 -0700

    added auth API utils

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {configureStore} from './store/store';
 import Root from './components/root';
+import {login, logout, signup} from './util/auth_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const store = configureStore;
-    ReactDOM.render(
-      <Root store={store}/>,
-      document.getElementById('root')
-    );
-  }
-);
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+  const store = configureStore;
+  ReactDOM.render(
+    <Root store={store}/>,
+    document.getElementById('root')
+  );
+});
