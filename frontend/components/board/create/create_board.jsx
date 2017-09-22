64e0496b0c8c89f751333fd682ede6a9e1c53902
John Hartman <john.f.d.hartman@gmail.com>
2017-09-22 11:22:50 -0700

    polling for cursor position every second with vanilla canvas

--- frontend/components/board/create/create_board.jsx
+++ frontend/components/board/create/create_board.jsx
@@ -1,21 +1,68 @@
 import React from 'react';
+import {findDOMNode} from 'react-dom';
 
 class CreateBoard extends React.Component {
   constructor(props){
     super(props);
-    this.path = [];
-    this.brush = {color: 'black', thickness: '3px'};
+    this.state = {
+      brush: {
+        thickness: '3px',
+        color: 'black'
+      },
+      pointer: {
+        x: 0,
+        y: 0
+      },
+      active: false
+    };
   }
 
+  componentDidMount(){
+    let canvas = findDOMNode(this.canvasRef);
+    let context = canvas.getContext('2d');
+    this.setState({
+      canvas: canvas,
+      context: context,
+    });
+    this.startDrawing.bind(this)();
+  }
+
+  startDrawing(){
+    setInterval( ()=>console.log(this.state.pointer),
+    30);
+  }
+
+
+
   handleMouseDown(event) {
-    
+    console.log(this.getPointerPos(event));
+  }
+
+  handleMouseMove(event) {
+    let {top, left} = this.state.canvas.getBoundingClientRect();
+    this.setState({
+      pointer: {
+        x: event.pageX - left,
+        y: event.pageY - top
+      }
+    });
+  }
+
+  getPointerPos(event) {
+    return {
+      x: this.state.pointer.x,
+      y: this.state.pointer.y
+    };
   }
 
   render() {
 
     return(
-      <canvas id='board-canvas'
-        onMouseDown={this.handleMouseDown.bind(this)}>
+      <canvas
+        ref={(canvas) => { this.canvasRef = canvas; }}
+        id='board-canvas'
+        onMouseDown={this.handleMouseDown.bind(this)}
+        onMouseMove={this.handleMouseMove.bind(this)}>
       </canvas>
     );
   }
