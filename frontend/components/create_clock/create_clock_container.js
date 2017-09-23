e6b949de398f68891e485274ca1c1e9c5a6473aa
John Hartman <john.f.d.hartman@gmail.com>
2017-09-23 16:24:29 -0700

    mapped action and state to clock props

--- frontend/components/create_clock/create_clock_container.js
+++ frontend/components/create_clock/create_clock_container.js
@@ -1,13 +1,15 @@
 
 import {connect} from 'react-redux';
 import CreateClock from './create_clock';
+import {updateCreateBoardStage} from '../../actions/create_board_actions';
 
 const mapStateToProps = (state) => ({
-
+  createBoardStage: state.createBoard.createBoardStage
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  updateCreateBoardStage: (stage) =>
+    dispatch(updateCreateBoardStage(stage))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateClock);
