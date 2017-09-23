203931f50040a83ce5400d45075a26c0a6ea7231
John Hartman <john.f.d.hartman@gmail.com>
2017-09-23 16:25:19 -0700

    added state and dispatch to props for create board

--- frontend/components/board/create/create_board_container.js
+++ frontend/components/board/create/create_board_container.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import CreateBoard from './create_board.jsx';
+import {updateCreateBoardStage} from '../../actions/create_board_actions';
 
 const mapStateToProps = (state) => ({
-
+  createBoardStage: state.createBoard.createBoardStage
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  updateCreateBoardStage: (stage) =>
+    dispatch(updateCreateBoardStage(stage))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateBoard);
