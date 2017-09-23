2de888c388a95e3fbdb55b92503dd9934366833e
John Hartman <john.f.d.hartman@gmail.com>
2017-09-23 16:20:12 -0700

    fixed naming error

--- frontend/reducers/create_board_reducer.js
+++ frontend/reducers/create_board_reducer.js
@@ -1,6 +1,6 @@
 import merge from 'lodash/merge';
 
-import {UPDATE_CREATE_BOARD_STAGE} from '../create_board_actions.js';
+import {UPDATE_CREATE_BOARD_STAGE} from '../actions/create_board_actions.js';
 
 export const createBoardReducer = (createBoardSlice = {}, action) => {
   Object.freeze(createBoardSlice);
