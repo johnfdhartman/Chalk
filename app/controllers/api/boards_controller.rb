7d9dc2a16d20936631f2e5448061308221bd7b17
John Hartman <john.f.d.hartman@gmail.com>
2017-09-24 13:23:40 -0700

    added code to boards controller

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -1,2 +1,14 @@
 class Api::BoardsController < ApplicationController
+  def create
+    @board = Board.new(title: params[:title],
+                       paths: params[:paths],
+                       author_id: current_user.id)
+
+    if @board.save
+      render 'api/boards/show'
+    else
+      @errors = @board.errors.full_messages
+      render 'api/boards/show', status: 422
+    end
+  end
 end
