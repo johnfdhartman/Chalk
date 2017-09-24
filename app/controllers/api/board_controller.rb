9f622eca916954f48dad7dff3243db461a2c63dd
John Hartman <john.f.d.hartman@gmail.com>
2017-09-24 13:19:35 -0700

    deleted incorrectly named Api::Board controller

--- app/controllers/api/board_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::BoardController < ApplicationController
-  def create
-    @board = Board.new(title: params[:title],
-                       paths: params[:paths],
-                       author_id: current_user.id)
-    
-  end
-end
