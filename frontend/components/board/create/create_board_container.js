d3fb9f22e55e435fc1c842294326ad221fe4c2ee
John Hartman <john.f.d.hartman@gmail.com>
2017-09-24 13:41:28 -0700

    added saveboard to createboard props

--- frontend/components/board/create/create_board_container.js
+++ frontend/components/board/create/create_board_container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import CreateBoard from './create_board.jsx';
-import {updateCreateBoardStage} from '../../../actions/create_board_actions';
+import {updateCreateBoardStage, saveBoard} from '../../../actions/create_board_actions';
 
 const mapStateToProps = (state) => ({
   createBoardStage: state.createBoard.createBoardStage
@@ -9,7 +9,10 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   updateCreateBoardStage: (stage) =>
-    dispatch(updateCreateBoardStage(stage))
+    dispatch(updateCreateBoardStage(stage)),
+
+  saveBoard: (board) =>
+    dispatch(saveBoard(board))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateBoard);
