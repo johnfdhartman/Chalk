8a97d3913bea3635c2dfc12b7cfc9104f7a46d62
John Hartman <john.f.d.hartman@gmail.com>
2017-09-24 13:07:33 -0700

    added activerecord associations for users and boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,11 @@
 class Board < ApplicationRecord
+  validates :title, :author_id, :paths, presence: true
+
+  belongs_to :author,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: 'User'
+
+  
+
 end
