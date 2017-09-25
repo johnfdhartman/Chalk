ac942b57a96835244526d270e7ed4d8c14b2106f
John Hartman <john.f.d.hartman@gmail.com>
2017-09-24 17:11:43 -0700

    added container and component for view board

---
+++ frontend/components/board/view/view_board_container.js
@@ -0,0 +1,13 @@
+import {connect} from 'react-redux';
+import ViewBoard from './view_board.jsx';
+
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ViewBoard);
