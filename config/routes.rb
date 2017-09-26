2ea2a811b887cc51607098cd92f71cb1f9a04774
John Hartman <john.f.d.hartman@gmail.com>
2017-09-26 14:13:31 -0700

    added api route for user boards

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create] do
-      resources :boards, only: [:index]
-    end
+    resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :boards, only: [:create, :show]
+
+    get 'users/:user_id/boards/:page', to: 'users#boards'
   end
 end
