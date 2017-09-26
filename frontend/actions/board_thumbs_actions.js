aa43fad91e6db0be2c99c142f3d675dfdd27dc9a
John Hartman <john.f.d.hartman@gmail.com>
2017-09-26 15:23:30 -0700

    added actions for board thumbs errors

--- frontend/actions/board_thumbs_actions.js
+++ frontend/actions/board_thumbs_actions.js
@@ -1,14 +1,21 @@
 import * as Api from '../util/board_thumbs_util';
 
 export const RECEIVE_BOARD_THUMBS = 'RECEIVE_BOARD_THUMBS';
+export const RECEIVE_BOARD_THUMBS_ERRORS = 'RECEIVE_BOARD_THUMBS_ERRORS';
 
 export const receiveBoardThumbs = boardThumbs => ({
   type: RECEIVE_BOARD_THUMBS,
   boardThumbs
 });
 
+export const receiveBoardThumbsErrors = boardThumbsErrors => ({
+  type: RECEIVE_BOARD_THUMBS_ERRORS,
+  boardThumbsErrors
+});
+
 export const requestUserBoardThumbs = (userId, page) => (dispatch) => (
   Api.fetchUserBoardThumbs(userId, page).then(
-    boardThumbs => dispatch(receiveBoardThumbs(boardThumbs))
+    boardThumbs => dispatch(receiveBoardThumbs(boardThumbs)),
+    errors => dispatch(receiveBoardThumbsErrors(errors))
   )
 );
