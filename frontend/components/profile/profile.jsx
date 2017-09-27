4902c77299f1b90d5c415c07e8d509131b216765
John Hartman <john.f.d.hartman@gmail.com>
2017-09-27 16:08:29 -0700

    added props to profile container

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
+import ThumbnailContainer from '../board/thumbnail/thumbnail-container';
 const Profile = ({props}) => {
-
-
   return(
     <div>
-      profile yes
+      
     </div>
   );
 };
--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -1,12 +1,16 @@
 import {connect} from 'react-redux';
 import Profile from './profile';
+import {requestUserBoardThumbs} from '../../actions/board_thumb_actions';
+
 
 const mapStateToProps = (state) => ({
-
+  session: state.session,
+  errors: state.errors
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  requestUserBoardThumbs: (userId, page) => (
+    dispatch(requestUserBoardThumbs(userId, page)))
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(Profile);
