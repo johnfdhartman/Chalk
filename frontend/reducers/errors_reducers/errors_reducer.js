18178d00fb637cfa9ee097914c71f451f999978b
John Hartman <john.f.d.hartman@gmail.com>
2017-09-27 18:23:28 -0700

    removed unneccesary boardthumbs state handlers

--- frontend/reducers/errors_reducers/errors_reducer.js
+++ frontend/reducers/errors_reducers/errors_reducer.js
@@ -1,10 +1,8 @@
 import {combineReducers} from 'redux';
 import {sessionErrorsReducer} from './session_errors_reducer';
 import {boardsErrorsReducer} from './boards_errors_reducer';
-import {boardThumbsErrorsReducer} from './board_thumbs_errors_reducer';
 
 export default combineReducers({
   sessionErrors: sessionErrorsReducer,
   boardsErrors: boardsErrorsReducer,
-  boardThumbsErrors: boardThumbsErrorsReducer
 });
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,6 @@
 import {sessionReducer} from './session_reducer';
 import errorsReducer from './errors_reducers/errors_reducer.js';
 import {boardsReducer} from './boards_reducer.js';
-import {boardThumbsReducer} from './board_thumbs_reducer';
 import {combineReducers} from 'redux';
 
 
@@ -9,6 +8,5 @@ const RootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
   boards: boardsReducer,
-  boardThumbs: boardThumbsReducer
 });
 export default RootReducer;
