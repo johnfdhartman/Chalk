f80111ef0b47054e66f9d9c2ec33c12c55a27086
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 11:45:20 -0700

    allowed navbar to display when not logged in

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -18,19 +18,9 @@ class Nav extends React.Component {
     });
   }
 
-
-  render() {
-    // if (this.props.location.pathname === '/create' ||
-    //     this.props.location.pathname.slice(0,4)) {
-    //   console.log('yeeee');
-    //   return (<div></div>);
-    // }
-    return(
-      <nav>
-        <div id='logo'>LOGO</div>
-        <input id='search'
-          value={this.state.inputVal}
-          onChange={this.handleInput.bind(this)}/>
+  renderSessionBar() {
+    if (this.props.currentUser) {
+      return (
         <div id='session-bar'>
           <div id='welcome'>
             Welcome, {this.props.currentUser.username}
@@ -41,13 +31,39 @@ class Nav extends React.Component {
             onClick={this.handleLogout.bind(this)}>
             LOGOUT
           </button>
+          <Link
+            to={`/users/${this.props.currentUser.id}`}
+            id='profile'
+            className='nav-button white-button'
+            />
         </div>
-        <Link
-          to={`/users/${this.props.currentUser.id}`}
-          id='profile'
-          className='nav-button white-button'
-          />
+      );
+    } else {
+      return (
+        <div id='session-bar'>
+          <div>
+            Login or sign up!
+            <Link
+              to={`/`}
+              id={'login-signup'}
+              className='nav-button white-button'/>
+          </div>
+        </div>
+      );
+    }
+  }
 
+
+
+  render() {
+    let sessionBar = this.renderSessionBar.bind(this)();
+    return(
+      <nav>
+        <div id='logo'>LOGO</div>
+        <input id='search'
+          value={this.state.inputVal}
+          onChange={this.handleInput.bind(this)}/>
+        {sessionBar}
       </nav>
     );
   }
