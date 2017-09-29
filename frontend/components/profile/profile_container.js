bc082c61d42b47ce6e0b68043f9db5a2f9b7ae84
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 23:05:25 -0700

    added user prop and dispatch to profile container

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -2,18 +2,22 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import Profile from './profile';
 import {requestUserBoards} from '../../actions/board_actions';
-
+import {requestUser} from '../../actions/user_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   session: state.session,
   errors: state.errors,
   boards: state.boards,
-  userId: ownProps.match.params.userId
+  userId: ownProps.match.params.userId,
+  user: state.users[ownProps.match.params.userId]
 });
 
 const mapDispatchToProps = (dispatch) => ({
   requestUserBoards: (userId, page) => (
-    dispatch(requestUserBoards(userId, page)))
+    dispatch(requestUserBoards(userId, page))),
+  requestUser: (userId) => (
+    dispatch(requestUser(userId))
+  )
 });
 
 export default withRouter(
