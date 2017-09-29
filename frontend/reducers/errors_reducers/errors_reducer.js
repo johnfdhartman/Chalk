e5c0dbc23c1f50d961363cdc02c76027d63fbd7b
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 22:47:03 -0700

    combined reducers

--- frontend/reducers/errors_reducers/errors_reducer.js
+++ frontend/reducers/errors_reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {sessionErrorsReducer} from './session_errors_reducer';
 import {boardsErrorsReducer} from './boards_errors_reducer';
+import {usersErrorsReducer} from './users_errors_reducer';
 
 export default combineReducers({
   sessionErrors: sessionErrorsReducer,
   boardsErrors: boardsErrorsReducer,
+  usersErrors: usersErrorsReducer
 });
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,7 @@
 import {sessionReducer} from './session_reducer';
 import errorsReducer from './errors_reducers/errors_reducer.js';
 import {boardsReducer} from './boards_reducer.js';
+import {usersReducer} from './users_reducer.js';
 import {combineReducers} from 'redux';
 
 
@@ -8,5 +9,7 @@ const RootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
   boards: boardsReducer,
+  users: usersReducer
 });
+
 export default RootReducer;
