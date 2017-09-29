52584c3bd0f74853ddce89bfed3a3b697a029085
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 23:02:40 -0700

    fixed minor bug where user requests would log users out

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,6 +1,6 @@
 import * as Api from '../util/auth_api_util';
 
-export const RECEIVE_CURRENT_USER = 'RECEIVE_USER';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = (user) => ({
