06064b13fe846f419e66d169115f37eab198d919
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 21:39:31 -0700

    storing thumbnails as state variables in profile

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -5,30 +5,48 @@ import ThumbnailContainer from '../board/thumbnail/board_thumbnail_container';
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      thumbnails: []
+    };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.props.requestUserBoards(this.props.userId, 1);
   }
 
-  renderThumbnails() {
-    let thumbnails = Object.values(this.props.boards);
-    return thumbnails.map( (thumbnail) => (
+  storeThumbnails(boards) {
+    let thumbnails = Object.values(boards);
+    let thumbnailComponents = thumbnails.map( (thumbnail) => (
       <div key={thumbnail.id}>
         <ThumbnailContainer boardId={thumbnail.id}/>
        </div>
     ));
+    this.setState({
+      thumbnails: thumbnailComponents
+    });
+    console.log('yeee');
+  }
+
+  componentWillReceiveProps(nextProps){
+    if (this.state.thumbnails.length === 0
+      && Object.values(nextProps.boards).length > 0) {
+      this.storeThumbnails.bind(this)(Object.values(nextProps.boards));
+    }
+  }
+
+  componentWillUnmount() {
+
   }
 
   render() {
-    let thumbnails = this.renderThumbnails.bind(this)();
+    console.log('this.state.thumbnails', this.state.thumbnails);
     return(
       <div className='profile'>
         <div className='user-info'>
           User Info!
         </div>
         <div className='thumbnails'>
-          {thumbnails}
+          {this.state.thumbnails}
         </div>
       </div>
     );
