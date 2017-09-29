592e33e6dedb82a942e729a8a2cf54029fc4936f
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 22:45:45 -0700

    added user errors reducer

---
+++ frontend/reducers/errors_reducers/users_errors_reducer.js
@@ -0,0 +1,18 @@
+import merge from 'lodash/merge';
+
+import {RECEIVE_USER_ERRORS} from '../../actions/user_actions';
+
+export const usersErrorsReducer = (userErrorsSlice, action) => {
+  Object.freeze(userErrorsSlice);
+  let newSlice;
+
+  switch(action.type) {
+    case RECEIVE_USER_ERRORS:
+      newSlice = merge({}, userErrorsSlice);
+      newSlice.push(action.userErrors);
+      return newSlice;
+
+    default:
+      return userErrorsSlice;
+  }
+};
