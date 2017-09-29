5a445355f3807d0ab3604cd76bf1822493a141d6
John Hartman <john.f.d.hartman@gmail.com>
2017-09-29 00:35:50 -0700

    oh no

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ThumbnailContainer from '../board/thumbnail/board_thumbnail_container';
-
+import merge from 'lodash/merge';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -42,20 +42,39 @@ class Profile extends React.Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    let nowBoards = Object.values(this.props.boards);
-    let nextBoards = Object.values(nextProps.boards);
-    return (!(nowBoards.length === nextBoards.length
-      && this.props.userId === nextProps.userId));
+    let nowBoards = Object.values(merge({},this.props.boards));
+    let nextBoards = Object.values(merge({},nextProps.boards));
+    let shouldUpdate = (!(
+      this.props.userId === nextProps.userId
+      && this.boardsAreEqual.bind(this)(nowBoards, nextBoards))
+    );
+    console.log('nowBoards', nowBoards);
+    console.log('nextBoards', nextBoards);
+    console.log('shouldUpdate', shouldUpdate);
+    return shouldUpdate;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.userId !== this.props.userId) {
-      this.setState({
-        thumbnails: []
-      });
-      this.props.requestUserBoards(this.props.userId, 1);
-      this.props.requestUser(this.props.userId);
-    }
+    // console.log('prevProps', prevProps);
+    // console.log('this.props', this.props);
+    // console.log('boardsAreEqual', (this.boardsAreEqual.bind(this)(prevProps.boards, this.props.boards)));
+    this.setState({
+      thumbnails: []
+    });
+    this.props.requestUserBoards(this.props.userId, 1);
+    this.props.requestUser(this.props.userId);
+    console.log('this.state', this.state);
+  }
+
+  boardsAreEqual(oldBoards, newBoards) {
+    //compares two arrays of boards
+    //returns true iff they are equal (comparing with board Id), not
+    //including order
+    let oldIds = oldBoards.map( (board) => (board.id)).sort();
+    let newIds = newBoards.map( (board) => (board.id)).sort();
+    console.log(`${oldBoards} === ${newBoards}?`,
+      (JSON.stringify(oldBoards) == JSON.stringify(newBoards)));
+    return (JSON.stringify(oldIds) == JSON.stringify(newIds));
   }
 
   renderUserInfo() {
@@ -71,6 +90,7 @@ class Profile extends React.Component {
   }
 
   render() {
+    console.log('rerendering');
     let userInfo = this.renderUserInfo.bind(this)();
     return(
       <div className='profile'>
