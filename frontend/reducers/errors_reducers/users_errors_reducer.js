2e70e65be6499a7b38fdf265b16186a2e32ff5d6
John Hartman <john.f.d.hartman@gmail.com>
2017-09-28 22:48:07 -0700

    added default empty objects for userserrorsreducer

--- frontend/reducers/errors_reducers/users_errors_reducer.js
+++ frontend/reducers/errors_reducers/users_errors_reducer.js
@@ -2,7 +2,7 @@ import merge from 'lodash/merge';
 
 import {RECEIVE_USER_ERRORS} from '../../actions/user_actions';
 
-export const usersErrorsReducer = (userErrorsSlice, action) => {
+export const usersErrorsReducer = (userErrorsSlice = {}, action) => {
   Object.freeze(userErrorsSlice);
   let newSlice;
 
