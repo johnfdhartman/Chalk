d1e54395824fb93fc595cfdf7c7713012e711f5c
John Hartman <john.f.d.hartman@gmail.com>
2017-10-18 14:40:37 -0700

    navbar is a ul

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -18,55 +18,81 @@ class Nav extends React.Component {
     });
   }
 
-  renderSessionBar() {
+  renderWelcome() {
     if (this.props.currentUser) {
       return (
-        <div className='session-bar'>
-          <div className='welcome-logout'>
-            <div className='welcome'>
-              Welcome, {this.props.currentUser.username}
-            </div>
-            <button
-              className='nav-button white-button logout'
-              onClick={this.handleLogout.bind(this)}>
-              LOGOUT
-            </button>
-          </div>
-          <Link
-            to={`/users/${this.props.currentUser.id}`}
-            className='nav-button white-button profile'
-            />
-        </div>
+        <li>
+          Welcome, {this.props.currentUser.username}
+        </li>
       );
     } else {
       return (
-        <div className='session-bar'>
-          <div>
+        <li>
 
-            <Link
-              to={`/`}
-              id={'login-signup'}
-              className='nav-button white-button'>
-              LOGIN/SIGNUP
-            </Link>
-          </div>
-        </div>
+        </li>
+      );
+    }
+  }
+
+  renderSessionButton() {
+    if (this.props.currentUser) {
+      return (
+        <li>
+          <button
+            className='nav-button white-button logout'
+            onClick={this.handleLogout.bind(this)}>
+            LOGOUT
+          </button>
+        </li>
+      );
+    } else {
+      return (
+        <li>
+          <Link
+            to={`/`}
+            id={'login-signup'}
+            className='nav-button white-button'>
+            LOGIN/SIGNUP
+          </Link>
+        </li>
       );
     }
   }
 
 
+  renderProfile() {
+    if (this.props.currentUser) {
+      return (
+        <li>
+          <Link
+            to={`/users/${this.props.currentUser.id}`}
+            className='nav-button white-button profile'
+            />
+        </li>
+      );
+    } else {
+      return (
+        null
+      );
+    }
+  }
 
   render() {
-    let sessionBar = this.renderSessionBar.bind(this)();
+    let welcome = this.renderWelcome.bind(this)();
+    let profile = this.renderProfile.bind(this)();
+    let sessionButton = this.renderSessionButton.bind(this)();
     return(
-      <nav>
-        <div id='logo'>LOGO</div>
-        <input id='search'
-          value={this.state.inputVal}
-          onChange={this.handleInput.bind(this)}/>
-        {sessionBar}
-      </nav>
+      <ul>
+        <li id='logo'>LOGO</li>
+        <li>
+          <input id='search'
+            value={this.state.inputVal}
+            onChange={this.handleInput.bind(this)}/>
+        </li>
+        {welcome}
+        {profile}
+        {sessionButton}
+      </ul>
     );
   }
 }
