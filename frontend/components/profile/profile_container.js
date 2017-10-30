d1634154926df15769e18649e7a227d2f95aad0b
John Hartman <john.f.d.hartman@gmail.com>
2017-10-30 14:30:49 +1000

    profile now receives requestUpdateUser, partially applied with the id of the user the profile belongs to

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -2,6 +2,7 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import Profile from './profile';
 import {requestUserBoards} from '../../actions/board_actions';
+import {requestUpdateUser} from '../../actions/user_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   session: state.session,
@@ -10,9 +11,12 @@ const mapStateToProps = (state, ownProps) => ({
   userId: ownProps.match.params.userId,
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch, ownProps) => ({
   requestUserBoards: (userId, page) => (
-    dispatch(requestUserBoards(userId, page)))
+    dispatch(requestUserBoards(page))),
+  requestUpdateUser: (userData) => (
+    dispatch(requestUpdateUser(ownProps.match.params.userId, userData))
+  )
 });
 
 export default withRouter(
