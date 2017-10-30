619f668b905b816fb4deef49633fd25889da3a4f
John Hartman <john.f.d.hartman@gmail.com>
2017-10-30 13:43:35 +1000

    added ajax method for updating user data

--- frontend/util/user_util.jsx
+++ frontend/util/user_util.jsx
@@ -1,7 +1,15 @@
 
-export const fetchUser = (id) => {
-  return $.ajax({
+export const fetchUser = id => (
+  $.ajax({
     url: `/api/users/${id}`,
     method: 'GET'
-  });
-};
+  })
+);
+
+export const updateUser = user => (
+  $.ajax({
+    url: `api/users/${user.id}`,
+    method: 'PUT',
+    data: user
+  })
+);
