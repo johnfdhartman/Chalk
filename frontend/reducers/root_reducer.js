75bf5a288f786438f1ebe024684a0a800f7ded0c
John Hartman <john.f.d.hartman@gmail.com>
2017-11-01 16:14:04 +1000

    combined ui reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,6 +2,7 @@ import {sessionReducer} from './session_reducer';
 import errorsReducer from './errors_reducers/errors_reducer.js';
 import {boardsReducer} from './boards_reducer.js';
 import {usersReducer} from './users_reducer.js';
+import {uiReducer} from './ui_reducer.js';
 import {combineReducers} from 'redux';
 
 
@@ -9,7 +10,8 @@ const RootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
   boards: boardsReducer,
-  users: usersReducer
+  users: usersReducer,
+  ui: uiReducer
 });
 
 export default RootReducer;
