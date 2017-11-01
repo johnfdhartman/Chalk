daf53b366a4d2549f6cf1ba77f8011c5aa4586ac
John Hartman <john.f.d.hartman@gmail.com>
2017-11-01 15:07:17 +1000

    added actions and reducer for ui slice

---
+++ frontend/actions/ui_actions.js
@@ -0,0 +1,10 @@
+export const OPEN_BIO_EDITOR = 'OPEN_BIO_EDITOR';
+export const CLOSE_BIO_EDITOR = 'CLOSE_BIO_EDITOR';
+
+export const openBioEditor = () => ({
+  type: OPEN_BIO_EDITOR
+});
+
+export const closeBioEditor = () => ({
+  type: CLOSE_BIO_EDITOR
+});
---
+++ frontend/reducers/ui_reducer.js
@@ -0,0 +1,26 @@
+import {
+  OPEN_BIO_EDITOR,
+  CLOSE_BIO_EDITOR
+} from '../actions/ui_actions.js';
+import merge from 'lodash/merge';
+
+export const uiReducer = (uiSlice = {}, action) => {
+  Object.freeze(uiSlice);
+  let newSlice;
+  switch(action.type) {
+    case OPEN_BIO_EDITOR:
+      newSlice = merge({}, uiSlice);
+      newSlice.ui.bio = newSlice.ui.bio || {};
+      newSlice.ui.bio.editing = true;
+      return newSlice;
+
+    case CLOSE_BIO_EDITOR:
+      newSlice = merge({}, uiSlice);
+      newSlice.ui.bio = newSlice.ui.bio || {};
+      newSlice.ui.bio.editing = false;
+      return newSlice;
+
+    default:
+      return uiSlice;
+  }
+};
