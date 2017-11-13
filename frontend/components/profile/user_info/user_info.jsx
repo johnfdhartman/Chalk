47d05fdf162989a3913d1f76a0699d6a9e2d391e
John Hartman <john.f.d.hartman@gmail.com>
2017-11-13 10:31:09 +1000

    work

--- frontend/components/profile/user_info/user_info.jsx
+++ frontend/components/profile/user_info/user_info.jsx
@@ -35,12 +35,13 @@ class UserInfo extends React.Component {
   }
 
   renderBio() {
+    console.log('this.props', this.props);
     if (!this.props.user ||
-      !this.props.user.bio ||
+      (this.props.user.bio === undefined) ||
         (!this.props.ui
-        || !this.props.ui.profile
-        || !this.props.ui.profile.bio
-        || !this.props.ui.profile.bio.editing
+        && !this.props.ui.profile
+        && !this.props.ui.profile.bio
+        && !this.props.ui.profile.bio.editing
       )) {
         return (
           this.renderDisplayBio()
